55c43a083f2eb3c605ac0a3232104bd37c3246aa
David Forer <david.j.forer@gmail.com>
2019-01-06 10:28:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-    if phrase == "I love you Grandma".upcase
+    if phrase == "I LOVE YOU GRANDMA"
       return 'I LOVE YOU TO PUMPKIN!'
   
     elsif phrase.upcase == phrase
